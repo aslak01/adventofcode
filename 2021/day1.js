2908739ee1c30987f9e4fae8f7b1cf4d4135160f
aslak <aslak.bakkeland@everyday.no>
2021-12-25 00:21:54 +0100

    day1

---
+++ 2021/day1.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var text = fs.readFileSync('./day1.txt', 'utf-8')
+var textByLine = text.split('\n')
+
+// textByLine = textByLine.map((n) => parseInt(n))
+
+let increases = 1
+for (let i = 0; i < textByLine.length; i++) {
+  if (i > 0) {
+    if (textByLine[i] > textByLine[i - 1]) {
+      increases++
+    }
+  }
+}
+
+console.log(increases)
