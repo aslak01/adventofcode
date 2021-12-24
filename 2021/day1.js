75e147782178f4263c84a6521e420ac6c6e6ce8c
aslak <aslak.bakkeland@everyday.no>
2021-12-25 00:34:01 +0100

    gitignoring puzzle inputs

--- 2021/day1.js
+++ 2021/day1.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var text = fs.readFileSync('./day1.txt', 'utf-8')
 var textByLine = text.split('\n')
 
-// textByLine = textByLine.map((n) => parseInt(n))
+textByLine = textByLine.map((n) => parseInt(n))
 
 let increases = 1
 for (let i = 0; i < textByLine.length; i++) {
