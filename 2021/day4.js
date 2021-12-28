a49b7758da0e7b1570107710092967731ef287ce
aslak <aslak.bakkeland@everyday.no>
2021-12-28 22:55:53 +0100

    start 4

--- 2021/day4.js
+++ 2021/day4.js
@@ -1,20 +1,78 @@
 const fs = require('fs')
-const lines = fs.readFileSync('./day4.txt', 'utf-8').trim().split('\n\n')
+const file = fs.readFileSync('./day4.txt', 'utf-8')
 
-let numbers = lines
-  .shift()
-  .split(',')
-  .map((x) => parseInt(x.trim(' ')))
+const day4pt1 = (file) => {
+  const lines = file.trim().split('\n\n')
 
-let boards = lines.map((x) => x.split('\n'))
+  const numbers = lines
+    .shift()
+    .split(',')
+    .map((n) => parseInt(n))
 
-let test = boards.map((x) => x.map((n) => n.split(' ').map((x) => parseInt(x))))
-test = test.map((x) => x.map((n) => n.filter((n) => n || n === 0)))
+  // boards split at newline
+  let boards = lines.map((x) => x.split('\n'))
+  // boards split at space and int parsed
+  boards = boards.map((x) => x.map((n) => n.split(' ').map((x) => parseInt(x))))
+  // removing NaNs from boards where single digits had leading zeroes
+  boards = boards.map((x) => x.map((n) => n.filter((n) => n || n === 0)))
 
-// test = test.map((b) => b.map((x) => x.map((n) => parseInt(n))))
+  // console.log(boards, numbers)
 
-console.log(boards[0])
+  const findBingo = (numbers, board, boardnumber) => {
+    const boardID = boardnumber
 
-console.log(test[0][0][0])
+    const findRows = (board) => {
+      const cols = board
+      let rows = []
+      for (let i = 0; i < board.length; i++) {
+        rows.push(board[i])
+      }
+      return { rows, cols }
+    }
+    const rows = findRows(board).rows
+    const cols = findRows(board).cols
 
-console.log(typeof test[0][0][0])
+    // console.log(rows, cols, boardID, board)
+    // const diag1 = board.map((x, i) => x[i])
+    // const diag2 = board.map((x, i) => x[board.length - 1 - i])
+    let winners
+    for (row of rows) {
+      if (
+        row.every((x) => numbers.includes(x))
+        // || diag1.every((x) => numbers.includes(x)) ||
+        // diag2.every((x) => numbers.includes(x)))
+      ) {
+        const numberIndexes = row.map((x) => numbers.indexOf(x))
+        const sumofIndexes = numberIndexes.reduce((a, b) => a + b)
+        winners = { boardID: { row, numberIndexes, sumofIndexes } }
+      }
+    }
+    for (col of cols) {
+      if (col.every((x) => numbers.includes(x))) {
+        const numberIndexes = col.map((x) => numbers.indexOf(x))
+        const sumofIndexes = numberIndexes.reduce((a, b) => a + b)
+        winners = { [boardID]: { col, numberIndexes, sumofIndexes } }
+      }
+      // console.log(winners)
+    }
+    return winners
+  }
+
+  const searchBoards = (numbers, boards) => {
+    let winners = []
+    for (const [index, board] of boards.entries()) {
+      winners.push(findBingo(numbers, board, index))
+    }
+    return winners
+  }
+  const winners = searchBoards(numbers, boards)
+  console.log(winners)
+  const findFirst = (array) =>
+    array.reduce((prev, curr) =>
+      prev.sumofIndexes < curr.sumofIndexes ? prev : curr
+    )
+  const firstWinner = findFirst(winners)
+  console.log('firstWinner', firstWinner)
+}
+
+day4pt1(file)
