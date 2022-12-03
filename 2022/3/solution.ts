b423bfb298e2f6b1abb5022652885670f77c4a76
aslak <aslakrb@gmail.com>
2022-12-04 00:17:41 +0100

    solve 3

--- 2022/3/solution.ts
+++ 2022/3/solution.ts
@@ -5,49 +5,6 @@ const rawPuzzle = await Deno.readTextFile("./input.txt");
 
 const puzzle = parse(rawPuzzle);
 
-const p1 = (input: string[]) => {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  const ALPHABET = upperCase(alphabet);
-
-  const isUpperCase = (string: string) => /^[A-Z]*$/.test(string);
-
-  const halfString = (string: string): [string, string] => {
-    const halfway = string.length / 2;
-    const firstHalf = string.slice(0, halfway);
-    const secondHalf = string.slice(-halfway);
-    return [firstHalf, secondHalf];
-  };
-
-  const matches = (a: string, b: string) => {
-    let match = "";
-    console.log(a, b);
-    a.split("").map((c: string) => {
-      if (b.includes(c)) {
-        match += c;
-      }
-    });
-    return match;
-  };
-
-  const countPoints = (string: string): number => {
-    let points = 0;
-    string.split("").map((c: string) => {
-      if (isUpperCase(c)) {
-        points += ALPHABET.indexOf(c) + alphabet.length + 1;
-      } else {
-        points += alphabet.indexOf(c) + 1;
-      }
-    });
-    return points;
-  };
-  const results = input.map((str: string) =>
-    countPoints(matches(...halfString(str)))
-  );
-
-  console.log(results);
-  return results.reduce((a: number, b: number) => a + b, 0);
-};
-
 const testData = [
   "vJrwpWtwJgWrhcsFMMfFFhFp",
   "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
@@ -57,7 +14,87 @@ const testData = [
   "CrZsJsPPZsGzwwsLwLmpwMDw",
 ];
 
-// 10883 too high
-console.log("p1", p1(puzzle.entries));
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const ALPHABET = upperCase(alphabet);
+
+const isUpperCase = (string: string) => /^[A-Z]*$/.test(string);
+
+const unqStrFromStr = (str: string[]): string =>
+  [...new Set(str.split(""))].toString();
+
+const unqStrFromArr = (arr: string[]): string => [...new Set(arr)].toString();
+
+const calcValue = (string): number => {
+  let points = 0;
+  if (isUpperCase(string)) {
+    points += ALPHABET.indexOf(string) + alphabet.length + 1;
+  } else {
+    points += alphabet.indexOf(string) + 1;
+  }
+  return points;
+};
+
+const p1 = (input: string[]) => {
+  const halfString = (string: string): [string, string] => {
+    const halfway = string.length / 2;
+    const firstHalf = string.slice(0, halfway);
+    const secondHalf = string.slice(-halfway);
+    return [firstHalf, secondHalf];
+  };
+
+  const matches = (a: string, b: string) => {
+    let match = "";
+    a.split("").map((c: string) => {
+      if (b.includes(c)) {
+        match += c;
+      }
+    });
+    if (match.length > 1) {
+      match = unqStrFromStr(match);
+    }
+    return match;
+  };
+
+  const countPoints = (string: string): number => {
+    if (string.length < 1) return 0;
+    const values = string.split("").map((c: string) => calcValue(c));
+    return values.reduce((a: number, b: number) => a + b, 0);
+  };
+
+  const results = input.map((str: string) =>
+    countPoints(matches(...halfString(str)))
+  );
+
+  return results.reduce((a: number, b: number) => a + b, 0);
+};
 
 console.log("p1test", p1(testData));
+console.log("p1", p1(puzzle.entries));
+
+const p2 = (input: string[]): number => {
+  const perGroup = 3;
+  const groups = input.reduce((resultArray, item, index) => {
+    const groupIndex = Math.floor(index / perGroup);
+    if (!resultArray[groupIndex]) {
+      resultArray[groupIndex] = []; // start a new group
+    }
+    resultArray[groupIndex].push(item);
+    return resultArray;
+  }, []);
+
+  const findCommon = (group: string[]): string => {
+    const strToArr = (str: string): string[] => str.split("");
+    const groupArrs = group.map(strToArr);
+    const join = groupArrs.reduce((join, current) =>
+      join.filter((el) => current.includes(el))
+    );
+    return unqStrFromArr(join);
+  };
+
+  const results = groups.map((g) => calcValue(findCommon(g)));
+  return results.reduce((a: number, b: number) => a + b, 0);
+};
+
+console.log("p2test", p2(testData));
+
+console.log("p2", p2(puzzle.entries));
