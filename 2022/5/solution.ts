3ae722f0f11892ee63798eb1895c4d06cdcf47f4
aslak <aslak.bakkeland@everyday.no>
2022-12-07 19:34:42 +0100

    5 fin

--- 2022/5/solution.ts
+++ 2022/5/solution.ts
@@ -1,5 +1,4 @@
 import { parse } from "../parse.ts";
-import { isUpperCase } from "../functions.ts";
 const rawPuzzle = await Deno.readTextFile("./input.txt");
 const puzzle = parse(rawPuzzle).entries;
 
@@ -12,7 +11,9 @@ const splitDataFromInstructions = (puzzle: string[]) => {
 };
 const splitPuzzle = splitDataFromInstructions(puzzle);
 
-const deserializeData = (data: string[]) => {
+type Stacks = string[][];
+
+const deserializeData = (data: string[]): Stacks => {
   const removeBrackets = (string: string) =>
     string.replaceAll("[", " ").replaceAll("]", " ");
   const splitByFours = (string: string) => {
@@ -21,11 +22,79 @@ const deserializeData = (data: string[]) => {
     if (match !== null) quadruplet = match;
     return quadruplet;
   };
-  const arrs = data
+  const lines = data
     .map((l) => splitByFours(removeBrackets(l)))
-    .map((l) => l.map((e) => e.trim()));
-  return arrs;
+    .map((l) => l.map((e) => e.trim()))
+    .filter((l) => isNaN(Number(l[0])) === true)
+    .reverse();
+
+  const transformToStacks = (lines: string[]): Stacks => {
+    const stacks: string[][] = [];
+    lines.map((l) =>
+      l.map((b, i: number) => {
+        if (b !== "") {
+          if (!stacks[i]) stacks[i] = [];
+          stacks[i].push(b);
+        }
+      })
+    );
+    return stacks;
+  };
+  const stacks = transformToStacks(lines);
+  return stacks;
 };
 const deserializedData = deserializeData(splitPuzzle.data);
-console.log(deserializedData);
-console.log(deserializedData.map((l) => l.length));
+
+const executeInstructions = (stacks: Stacks, instructions: string[]) => {
+  const deserializeInstructions = (instructions: string[]) => {
+    const moves = instructions
+      .map((l) => l.split(" "))
+      .map((l) => l.map((w) => w.replace(/\D/g, "")).filter(Boolean))
+      .map((l) => l.map((n) => Number(n)));
+    return moves;
+  };
+  // Moves: Amount, from, to
+  const moves = deserializeInstructions(instructions);
+  const executeMoves = (stks: Stacks, moves: number[][]) => {
+    const movingStacks = JSON.parse(JSON.stringify(stks)),
+      movingStacks9001 = JSON.parse(JSON.stringify(stks));
+    const moveCrate = (from: number, to: number) => {
+      const crate = movingStacks[from].pop();
+      if (typeof crate === "undefined") return;
+      movingStacks[to].push(crate);
+    };
+    const moveCrates9001 = (from: number, to: number, amount: number) => {
+      const crates = movingStacks9001[from].splice(-amount, amount);
+      movingStacks9001[to] = movingStacks9001[to].concat(crates);
+    };
+    moves.forEach((l) => {
+      const amount = l[0],
+        from = l[1] - 1,
+        to = l[2] - 1;
+      moveCrates9001(from, to, amount);
+      for (let i = 0; i < amount; i++) {
+        moveCrate(from, to);
+      }
+    });
+    return { 1: movingStacks, 2: movingStacks9001 };
+  };
+  const result = executeMoves(stacks, moves);
+  return result;
+};
+
+const result = executeInstructions(deserializedData, splitPuzzle.instructions);
+
+const serializeSolution = (result: { [key: string]: string[][] }) => {
+  let letters = [];
+  Object.values(result).map((l, i) => {
+    letters.push(" " + i + 1 + ": ");
+    l.map((c) => {
+      const letter = c.pop();
+      letters.push(letter);
+    });
+  });
+  return letters.join("");
+};
+const solution = serializeSolution(result);
+
+console.log(solution);
