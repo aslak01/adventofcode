29ab8f3edfe6f584eeb8f89a29d62a9e536b9af8
aslak <aslak.bakkeland@everyday.no>
2022-12-08 11:56:41 +0100

    start 7

--- 2022/7/solution.ts
+++ 2022/7/solution.ts
@@ -1,3 +1,7 @@
 import { parse } from "../parse.ts";
 const rawPuzzle = await Deno.readTextFile("./input.txt");
-const puzzle = parse(rawPuzzle).entries
+const puzzle = parse(rawPuzzle).entries;
+
+const example = parse(await Deno.readTextFile("./example.txt")).entries;
+
+console.log(example);
