2319e58afd160ba25b06383c1877e2c7fb175f1e
as <aslak.bakkeland@medialaben.no>
2024-12-24 14:28:35 +0100

    init day 8

---
+++ 2024/day8/main.ts
@@ -0,0 +1,20 @@
+import { solve_a, solve_b } from "./solution";
+
+const loadFile = async (path: string) => (await Bun.file(path).text()).trim();
+
+async function main() {
+  const input = await loadFile("input.txt");
+  const test = await loadFile("test.txt");
+  const test2 = await loadFile("test2.txt");
+
+  const a = Bun.argv[2] !== "b" && Bun.argv[3] !== "b";
+  const testMode = Bun.argv[2] === "test";
+
+  const testData = a ? test : test2;
+  const data = testMode ? testData : input;
+  const solve = a ? solve_a : solve_b;
+
+  console.log(solve(data));
+}
+
+main();
